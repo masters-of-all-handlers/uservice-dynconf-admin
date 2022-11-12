30bb9892b5d79285c9e002684b3da9979f5ff0e5
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-12 20:45:09 +0300

    добавлена фильтрация по имени

--- src/components/VarsTable/VarsTable.jsx
+++ src/components/VarsTable/VarsTable.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Table } from 'antd';
+import React, { useState } from 'react';
+import { Table, Input, Space } from 'antd';
 
 import styles from './styles.module.scss';
-import { sortByName } from './helpers';
+import { sortByName, filterByName } from './helpers';
 
 import VarsTableActions from '../VarsTableActions/VarsTableActions';
 import data from '../../__mocks__/data';
@@ -10,25 +10,57 @@ import data from '../../__mocks__/data';
 const { Column } = Table;
 
 const VariablesTable = () => {
+  const [dataSource, setDataSource] = useState(data);
+  const [searchText, setSearchText] = useState('');
+
+  const handleFilterByName = (e) => {
+    const text = e.target.value.toLowerCase();
+    setSearchText(text);
+
+    const filteredData = filterByName(text, data);
+    setDataSource(filteredData);
+  };
+
+  const renderColumnActions = (_, render) => (
+    <VarsTableActions render={render} />
+  );
+
   return (
-    <Table className={styles.table} dataSource={data} size="small">
-      <Column
-        title="Имя"
-        dataIndex="name"
-        key="name"
-        sorter={sortByName}
-        sortDirections={['descend']}
-      />
+    <>
+      <Space direction="vertical" size="middle" className={styles.wrap}>
+        <Input
+          placeholder="Поиск по имени"
+          value={searchText}
+          onChange={handleFilterByName}
+        />
 
-      <Column title="Сервис" dataIndex="service" key="service" />
+        <Table dataSource={dataSource} size="small">
+          <Column
+            title="Имя"
+            dataIndex="name"
+            key="name"
+            width="60%"
+            sorter={sortByName}
+            sortDirections={['descend']}
+          />
 
-      <Column
-        title="Действия"
-        key="actions"
-        align="right"
-        render={(_, render) => <VarsTableActions render={render} />}
-      />
-    </Table>
+          <Column
+            title="Сервис"
+            dataIndex="service"
+            key="service"
+            width="20%"
+          />
+
+          <Column
+            title="Действия"
+            key="actions"
+            align="right"
+            width="20%"
+            render={renderColumnActions}
+          />
+        </Table>
+      </Space>
+    </>
   );
 };
 
