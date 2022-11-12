a5ff7d90738492cc994bc0015ebe999bf4eca889
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-12 16:39:20 +0300

    добавлен вывод главной страницы

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+
+import HomePage from './pages/HomePage/HomePage';
 
 const App = () => {
-  return <div>App</div>;
+  return <HomePage />;
 };
 
 export default App;
