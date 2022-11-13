f76259199222b4a398313508c85c0c9744f222ba
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-13 19:14:49 +0300

    рефакторинг действий над параметром

--- src/components/VarsTableActions/VarsTableActions.jsx
+++ src/components/VarsTableActions/VarsTableActions.jsx
@@ -1,40 +1,24 @@
-import React from 'react';
-import { Button, Dropdown } from 'antd';
+import React from "react";
+import {Button, Dropdown} from "antd";
 import {
   EllipsisOutlined,
   EditOutlined,
   DeleteOutlined,
-} from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
+} from "@ant-design/icons";
+import {useNavigate} from "react-router-dom";
 
-const menu = {
-  items: [
-    {
-      key: 'edit',
-      label: 'Редактировать',
-      icon: <EditOutlined />,
-    },
-    {
-      key: 'delete',
-      label: 'Удалить',
-      icon: <DeleteOutlined />,
-      danger: true,
-    },
-  ],
-};
-
-const VarsTableActions = ({ render: { uuid } }) => {
+const VarsTableActions = ({render: {uuid}}) => {
   const navigate = useNavigate();
 
-  const handleMenuClick = (e, uuid) => {
-    const { key } = e;
+  const handleMenuClick = (e) => {
+    const {key} = e;
 
     switch (key) {
-      case 'edit':
+      case "edit":
         navigate(`/edit/${uuid}`);
         break;
 
-      case 'delete':
+      case "delete":
         console.log(`Удаление параметра ${uuid}`);
         break;
 
@@ -42,11 +26,25 @@ const VarsTableActions = ({ render: { uuid } }) => {
     }
   };
 
+  const menu = {
+    items: [
+      {
+        key: "edit",
+        label: "Редактировать",
+        icon: <EditOutlined />,
+      },
+      {
+        key: "delete",
+        label: "Удалить",
+        icon: <DeleteOutlined />,
+        danger: true,
+      },
+    ],
+    onClick: handleMenuClick,
+  };
+
   return (
-    <Dropdown
-      menu={{ ...menu, onClick: (e) => handleMenuClick(e, uuid) }}
-      trigger="click"
-    >
+    <Dropdown menu={menu} trigger="click">
       <Button type="text" icon={<EllipsisOutlined />} size="small" />
     </Dropdown>
   );
