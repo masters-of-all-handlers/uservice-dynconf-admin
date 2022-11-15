f7566483b303f8c752b646d119c15ed5822cb7d5
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-15 12:31:03 +0300

    добавлена кнопка создать переменную

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Layout, PageHeader } from 'antd';
+import React from "react";
+import {Layout, PageHeader, Button} from "antd";
+import {useNavigate} from "react-router-dom";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
-import VarsTable from '../../components/VarsTable/VarsTable';
+import VarsTable from "../../components/VarsTable/VarsTable";
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content, Footer} = Layout;
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleCreateClick = (e) => {
+    navigate(`/create`);
+  };
+
   return (
     <>
       <Layout className={styles.layout}>
         <Header className={styles.header}>Динамические конфиги Userver</Header>
 
         <Content className={styles.content}>
-          <PageHeader title="Список переменных" />
+          <PageHeader
+            title="Список переменных"
+            extra={[
+              <Button
+                key="createVar"
+                type="primary"
+                onClick={handleCreateClick}
+              >
+                Создать переменную
+              </Button>,
+            ]}
+          />
 
           <VarsTable />
         </Content>
