cc57d0052b85c7521d39919cbf3beace6b2bff9a
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-16 17:49:36 +0300

    уменьшен отступ

--- src/services/VariableService.js
+++ src/services/VariableService.js
@@ -1,60 +1,63 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const variableAPI = createApi({
-    reducerPath: 'variableAPI',
-    baseQuery: fetchBaseQuery({
-        baseUrl:
-            'https://my-json-server.typicode.com/masters-of-all-handlers/uservice-dynconf-admin',
-    }),
-    tagTypes: ['Variables'],
+  reducerPath: "variableAPI",
+  baseQuery: fetchBaseQuery({
+    baseUrl:
+      "https://my-json-server.typicode.com/masters-of-all-handlers/uservice-dynconf-admin",
+  }),
+  tagTypes: ["Variables"],
 
-    endpoints: (build) => ({
-        fetchAllVariables: build.query({
-            query: (limit = 10) => ({
-                url: '/variables',
-                params: {
-                    _limit: limit,
-                },
-            }),
-            providesTags: (result) => ['Variables'],
-        }),
-        fetchVariableById: build.query({
-            query: (id) =>
-                ({
-                    url: "/variable",
-                    params: {
-                        id
-                    }
-                }),
-            providesTags: result => {
-                return [`variable${result.id}`];
-            }
-        }),
-        updateVariable: build.mutation({
-            query: ({id, ...update}) => ({
-                url: "/variable",
-                method: "PATCH",
-                body: update,
-            }),
-            invalidatesTags: result => {
-                return [`variable${result.id}`];
-            }
-        }),
-        createVariable: build.mutation({
-            query: (variable) => ({
-                url: "/variables",
-                method: "POST",
-                body: variable
-            }),
-            invalidatesTags: ["variables"]
-        }),
-        deleteVariableById: build.mutation({
-            query: (uuid) => ({
-                url: `/variable/${uuid}`,
-                method: "DELETE",
-            }),
+  endpoints: (build) => ({
+    fetchAllVariables: build.query({
+      query: (limit = 10) => ({
+        url: "/variables",
+        params: {
+          _limit: limit,
+        },
+      }),
 
-            invalidatesTags: ["Variables"]
-        })
+      providesTags: (result) => ["Variables"],
     }),
+    fetchVariableById: build.query({
+      query: (id) => ({
+        url: "/variable",
+        params: {
+          id,
+        },
+      }),
+
+      providesTags: (result) => {
+        return [`variable${result.id}`];
+      },
+    }),
+    updateVariable: build.mutation({
+      query: ({id, ...update}) => ({
+        url: "/variable",
+        method: "PATCH",
+        body: update,
+      }),
+
+      invalidatesTags: (result) => {
+        return [`variable${result.id}`];
+      },
+    }),
+    createVariable: build.mutation({
+      query: (variable) => ({
+        url: "/variables",
+        method: "POST",
+        body: variable,
+      }),
+
+      invalidatesTags: ["Variables"],
+    }),
+    deleteVariableById: build.mutation({
+      query: (uuid) => ({
+        url: `/variable/${uuid}`,
+        method: "DELETE",
+      }),
+
+      invalidatesTags: ["Variables"],
+    }),
+  }),
 });
