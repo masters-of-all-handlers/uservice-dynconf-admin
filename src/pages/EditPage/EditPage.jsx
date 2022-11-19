5e3917bbf846de600401a9310e0a506ae33a79b7
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-19 16:39:22 +0300

    добавлен лейаут на edit page

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import {
-  Layout, message,
-} from 'antd';
-
-import styles from './styles.module.scss';
-import {variableAPI} from "../../services/VariableService";
+import React from "react";
+import {message} from "antd";
 import {useParams} from "react-router-dom";
-import EditVarForm from "../../components/EditVarForm/EditvarForm";
 
-const {Header, Content, Footer} = Layout;
+import MainLayout from "../MainLayout/MainLayout";
+
+import {variableAPI} from "../../services/VariableService";
+import EditVarForm from "../../components/EditVarForm/EditvarForm";
 
 const EditPage = () => {
   const {uuid} = useParams();
@@ -19,37 +16,25 @@ const EditPage = () => {
     isLoading: isLoadingVariable,
   } = variableAPI.useFetchVariableByIdQuery(uuid);
 
-  const [
-    updateVariable, {
-      error: updateError,
-      isLoading: isUpdateLoading,
-    }
-  ] = variableAPI.useUpdateVariableMutation();
+  const [updateVariable, {error: updateError, isLoading: isUpdateLoading}] =
+    variableAPI.useUpdateVariableMutation();
 
-  return <>
-    <Layout className={styles.layout}>
-      <Header className={styles.header}>Динамические конфиги
-        Userver</Header>
-      <Content className={styles.content}>
-        <EditVarForm
-          isLoading={isLoadingVariable}
-          isSaveLoading={isLoadingVariable || isUpdateLoading}
-          title="Редактировать переменную"
-          onFinish={
-            data => {
-              updateVariable(data).then(() => {
-                message.success("Сохранено");
-              })
-            }
-          }
-          initialValues={variableData}
-          error={variableError || updateError}
-        />
-      </Content>
-      <Footer className={styles.footer}>Сделано с любовью ❤️
-        2022</Footer>
-    </Layout>
-  </>;
+  return (
+    <MainLayout>
+      <EditVarForm
+        isLoading={isLoadingVariable}
+        isSaveLoading={isLoadingVariable || isUpdateLoading}
+        title="Редактировать переменную"
+        onFinish={(data) => {
+          updateVariable(data).then(() => {
+            message.success("Сохранено");
+          });
+        }}
+        initialValues={variableData}
+        error={variableError || updateError}
+      />
+    </MainLayout>
+  );
 };
 
 export default EditPage;
