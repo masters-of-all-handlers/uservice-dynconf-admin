f6314ecdd7fb6ed3e99b11c35f185f759f9708f5
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-20 17:53:22 +0300

    api и endpoint'ы перенесены в константы

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+// "https://my-json-server.typicode.com/masters-of-all-handlers/uservice-dynconf-admin",
+export const API_BASE_URL = "http://10.21.0.234:8083";
+export const API_BASE_ADMIN_URL = `${API_BASE_URL}/admin/v1`;
+export const API_CONFIGS_ENDPOINT = "/variables";
--- src/services/constants.js
+++
@@ -1,3 +0,0 @@
-// "https://my-json-server.typicode.com/masters-of-all-handlers/uservice-dynconf-admin",
-export const API_BASE_URL = "http://10.21.0.234:8080";
-export const CONFIGS_ENDPOINT = "/variables";
