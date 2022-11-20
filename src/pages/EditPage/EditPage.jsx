f483e3c0a6b8296c5433b7e654253e99719c6d78
Михаил Кормановский <kormanowsky@gmail.com>
2022-11-20 10:49:22 +0300

    убрал лишний import

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Layout, message,
+  message,
 } from 'antd';
 import {variableAPI} from "../../services/VariableService";
 import {useParams} from "react-router-dom";
