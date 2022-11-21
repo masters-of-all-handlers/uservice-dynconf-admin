7f7b04e91d649c1836e52d1b27477a8aefd657ba
Михаил Кормановский <kormanowsky@gmail.com>
2022-11-21 21:27:54 +0300

    небольшой фикс лого в меню

--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -7,14 +7,17 @@ import {DASHBOARD_CONFIGS_URL} from "../../constants";
 
 import {ReactComponent as Logo} from "../../logo.svg";
 import MainMenu from "../../components/MainMenu/MainMenu";
+import useAuth from "../../hooks/useAuth";
 
 const {Header, Content, Footer} = Layout;
 
 const MainLayout = ({children}) => {
+  const {data} = useAuth();
   return (
     <Layout className={styles.layout}>
       <Header className={styles.header}>
-        <Link className={styles.logo} to="/">
+        <Link className={styles.logo}
+              to={data.ticket ? DASHBOARD_CONFIGS_URL : "/"}>
           <Logo className={styles.logo_img}/>
           <div className={styles.logo_desc}>Динамические конфиги userver</div>
         </Link>
