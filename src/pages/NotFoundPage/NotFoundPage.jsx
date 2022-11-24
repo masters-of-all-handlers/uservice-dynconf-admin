d40da795e409201d3e50e497c9222406bc4ecfdd
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-24 16:08:07 +0300

    рефакторинг NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,28 +1,33 @@
-import {Button, Col, Layout, Row, Space, Typography} from "antd";
+import React from "react";
 import {Link, useNavigate} from "react-router-dom";
+import {Button, Space} from "antd";
+
 import styles from "./styles.module.scss";
-import {ReactComponent as Logo} from "../../logo.svg";
+
+import MainLayout from "../MainLayout/MainLayout";
+
+import LogoSection from "../../components/LogoSection/LogoSection";
 
 export default function NotFoundPage() {
   const navigate = useNavigate();
-  return <Layout className={styles.layout}>
-    <Row align="middle" className={styles.logoRow}>
-      <Col>
-        <Logo className={styles.logo}/>
-      </Col>
-      <Col>
-        <Typography.Title className={styles.notFound404}>404</Typography.Title>
-      </Col>
-    </Row>
-    <Typography.Title level={4}>По этому адресу ничего нет</Typography.Title>
-    <Space wrap>
-      <Link to="/login">
-        <Button type="primary">Войти</Button>
-      </Link>
-      <Link to="/">
-        <Button>На главную</Button>
-      </Link>
-      <Button onClick={() => navigate(-1)}>Назад</Button>
-    </Space>
-  </Layout>
+
+  const handleBackClick = () => navigate(-1);
+
+  return (
+    <MainLayout type="branded">
+      <LogoSection
+        title="404"
+        description="Похоже вы заблудились :("
+        logoType="gray"
+      >
+        <Space className={styles.buttons}>
+          <Button onClick={handleBackClick}>Назад</Button>
+
+          <Link to="/">
+            <Button type="primary">На главную</Button>
+          </Link>
+        </Space>
+      </LogoSection>
+    </MainLayout>
+  );
 }
