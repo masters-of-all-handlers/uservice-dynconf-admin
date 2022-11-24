77f2d61b96a9f866982c0ae276772fbaffdeb218
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-24 10:29:05 +0300

    рефакторинг MainLayout

--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -3,30 +3,32 @@ import {Layout} from "antd";
 import {Link} from "react-router-dom";
 
 import styles from "./styles.module.scss";
-import {DASHBOARD_CONFIGS_URL} from "../../constants";
 
+import {DASHBOARD_CONFIGS_URL} from "../../constants";
 import {ReactComponent as Logo} from "../../logo.svg";
-import MainMenu from "../../components/MainMenu/MainMenu";
 import useAuth from "../../hooks/useAuth";
+import MainMenu from "../../components/MainMenu/MainMenu";
 
 const {Header, Content, Footer} = Layout;
 
 const MainLayout = ({children}) => {
-  const {data} = useAuth();
+  const {
+    data: {ticket},
+  } = useAuth();
+
   return (
     <Layout className={styles.layout}>
       <Header className={styles.header}>
-        <Link className={styles.logo}
-              to={data.ticket ? DASHBOARD_CONFIGS_URL : "/"}>
-          <Logo className={styles.logo_img}/>
+        <Link className={styles.logo} to={ticket ? DASHBOARD_CONFIGS_URL : "/"}>
+          <Logo className={styles.logo_img} />
           <div className={styles.logo_desc}>Динамические конфиги userver</div>
         </Link>
-        <MainMenu/>
+        <MainMenu />
       </Header>
 
       <Content className={styles.content}>{children}</Content>
 
-      <Footer className={styles.footer}>2022 ❤️Сделано с любовью</Footer>
+      <Footer className={styles.footer}>2022 ❤️ Сделано с любовью</Footer>
     </Layout>
   );
 };
