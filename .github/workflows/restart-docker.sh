198ffd2b1b547b37fa06c4650f6c7e327691c058
Михаил Кормановский <kormanowsky@gmail.com>
2022-11-25 13:11:19 +0300

    исправил фильтр в restart-docker.sh

--- .github/workflows/restart-docker.sh
+++ .github/workflows/restart-docker.sh
@@ -2,5 +2,5 @@
 # $1 - имя образа
 # $2 - строка с портом (80:80, например)
 docker pull "$1"
-docker ps -f "$1" --format '{{.ID}}' | xargs docker stop
+docker ps -f "ancestor=$1" --format '{{.ID}}' | xargs docker stop
 docker run -d -p $2 "$1"
