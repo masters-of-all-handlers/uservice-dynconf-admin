fbf82d8df510890e752ad659b326545ff78ce8ee
YuriyLysyuk <lysyuk.y@yandex.ru>
2022-11-25 14:29:37 +0300

    добавлена проверка вывода асинк ошибки

--- src/middlewares/rtkQueryErrorLogger.js
+++ src/middlewares/rtkQueryErrorLogger.js
@@ -6,12 +6,15 @@ export const rtkQueryErrorLogger = (api) => (next) => (action) => {
   if (action?.type.includes("authAPI")) {
     return next(action);
   }
+
   if (isRejectedWithValue(action)) {
     const {
       payload: {status, error},
     } = action;
 
-    message.error(`Асинхронная ошибка ${status} | ${error}`);
+    const errorDetails = Boolean(error) ? ` | ${error}` : "";
+
+    message.error(`Асинхронная ошибка ${status}${errorDetails}`);
   }
 
   return next(action);
