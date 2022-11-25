1b0b8250b7fbe01ad53698874ef086aec6b8ee2a
Михаил Кормановский <kormanowsky@gmail.com>
2022-11-25 14:06:49 +0300

    добавил остановку контейнеров по используемому порту

--- .github/workflows/restart-docker.sh
+++ .github/workflows/restart-docker.sh
@@ -1,6 +1,7 @@
 # перезапуск контейнера
 # $1 - имя образа
-# $2 - строка с портом (80:80, например)
+# $2 - исходный порт
+# $3 - порт контейнера
 docker pull "$1"
-docker ps -f "ancestor=$1" --format '{{.ID}}' | xargs docker stop
-docker run -d -p $2 "$1"
+docker ps -f "publish=$2" --format '{{.ID}}' | xargs docker stop
+docker run -d -p $2:$3 "$1"
