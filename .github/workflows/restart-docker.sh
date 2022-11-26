916af8da67a4b2c4c00207dde8026c650efaef0c
Михаил Кормановский <kormanowsky@gmail.com>
2022-11-26 15:10:48 +0300

    обновил restart-docker.sh, временно включил на не мастере

--- .github/workflows/restart-docker.sh
+++ .github/workflows/restart-docker.sh
@@ -3,5 +3,5 @@
 # $2 - исходный порт
 # $3 - порт контейнера
 docker pull "$1"
-docker ps -f "publish=$2" --format '{{.ID}}' | xargs docker stop
-docker run -d -p $2:$3 "$1"
+cd ~/uservice-dynconf-deploy/
+docker-compose up --build -d --force-recreate webapp
